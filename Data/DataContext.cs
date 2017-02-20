e00b04800f659fbd86d497e8b9e3b8a6f274fb40
Quinntyne Brown <quinntynebrown@gmail.com>
2017-02-19 21:42:25 -0500

    MediatR

---
+++ Data/DataContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AspNetCoreJavaScriptServicesGettingStarted.Data
+{
+    public class DataContext
+    {
+    }
+}
---
+++ ServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MediatR;
+
+namespace AspNetCoreJavaScriptServicesGettingStarted
+{
+    public static class ServiceCollectionExtensions
+    {
+
+        public static IServiceCollection AddCustomMediator(this IServiceCollection services)
+        {
+            services.AddMediatR(typeof(Startup));
+            return services;
+        }
+    }
+}
